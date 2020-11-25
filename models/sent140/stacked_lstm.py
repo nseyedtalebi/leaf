52e2bd2d01431d93e78ef429c5d2693c798a2e0b
Nima Seyedtalebi <nima.seyedtalebi@uky.edu>
2020-11-25 16:29:23 -0500

    Added changes for DP taken from other branches

--- models/sent140/stacked_lstm.py
+++ models/sent140/stacked_lstm.py
@@ -4,17 +4,32 @@ import sys
 import tensorflow as tf
 
 from tensorflow.contrib import rnn
+from tensorflow_privacy.privacy.optimizers.dp_optimizer import (
+    DPGradientDescentGaussianOptimizer,
+)
 
 from model import Model
 from utils.language_utils import line_to_indices, get_word_emb_arr, val_to_vec
 
 
-VOCAB_DIR = 'sent140/embs.json'
+VOCAB_DIR = "sent140/embs.json"
 
 
 class ClientModel(Model):
-
-    def __init__(self, seed, lr, seq_len, num_classes, n_hidden, emb_arr=None):
+    def __init__(
+        self,
+        seed,
+        lr,
+        seq_len,
+        num_classes,
+        n_hidden,
+        l2_norm_clip,
+        noise_multiplier,
+        num_microbatches=None,
+        unroll_microbatches=False,
+        emb_arr=None,
+        ledger=None,
+    ):
         self.seq_len = seq_len
         self.num_classes = num_classes
         self.n_hidden = n_hidden
@@ -22,29 +37,43 @@ class ClientModel(Model):
         self.vocab_size = len(vocab)
         if emb_arr:
             self.emb_arr = emb_arr
-        super(ClientModel, self).__init__(seed, lr)
+        dp_opt = DPGradientDescentGaussianOptimizer(
+            l2_norm_clip,
+            noise_multiplier,
+            num_microbatches=None,
+            ledger=None,
+            unroll_microbatches=False,
+            learning_rate=lr,
+        )
+        self.dp_delta = None
+        self.dp_epsilon = None
+        super(ClientModel, self).__init__(seed, lr, dp_opt)
 
     def create_model(self):
         features = tf.placeholder(tf.int32, [None, self.seq_len])
         embedding = tf.get_variable(
-            'embedding', [self.vocab_size + 1, self.n_hidden], dtype=tf.float32)
+            "embedding", [self.vocab_size + 1, self.n_hidden], dtype=tf.float32
+        )
         x = tf.cast(tf.nn.embedding_lookup(embedding, features), tf.float32)
         labels = tf.placeholder(tf.float32, [None, self.num_classes])
-        
+
         stacked_lstm = rnn.MultiRNNCell(
-            [rnn.BasicLSTMCell(self.n_hidden) for _ in range(2)])
+            [rnn.BasicLSTMCell(self.n_hidden) for _ in range(2)]
+        )
         outputs, _ = tf.nn.dynamic_rnn(stacked_lstm, x, dtype=tf.float32)
         fc1 = tf.layers.dense(inputs=outputs[:, -1, :], units=128)
         pred = tf.layers.dense(inputs=fc1, units=self.num_classes)
-        
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=labels))
+
+        loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=labels)
+        )
         train_op = self.optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        
+            loss=loss, global_step=tf.train.get_global_step()
+        )
+
         correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(labels, 1))
         eval_metric_ops = tf.count_nonzero(correct_pred)
-        
+
         return features, labels, train_op, eval_metric_ops, loss
 
     def process_x(self, raw_x_batch, max_words=25):
@@ -58,3 +87,28 @@ class ClientModel(Model):
         y_batch = [val_to_vec(self.num_classes, e) for e in y_batch]
         y_batch = np.array(y_batch)
         return y_batch
+
+    def train(self, data, num_epochs=1, batch_size=10):
+        """
+        Trains the client model.
+        Args:
+        data: Dict of the form {'x': [list], 'y': [list]}.
+        num_epochs: Number of epochs to train.
+        batch_size: Size of training batches.
+        Return:
+        comp: Number of FLOPs computed while training given data
+        update: List of np.ndarray weights, with each weight array
+            corresponding to a variable in the resulting graph
+        """
+        noise = self.optimizer.noise_multiplier
+        self.dp_delta = 1 / len(data["y"])  # using this for N
+        self.dp_epsilon = None
+        for epoch_num in range(num_epochs):
+            self.run_epoch(data, batch_size)
+            # Add DP stuff
+            self.dp_epsilon = compute_dp_sgd_privacy(
+                len(data["y"]), batch_size, noise, epoch_num, dp_delta
+            )
+        update = self.get_params()
+        comp = num_epochs * (len(data["y"]) // batch_size) * batch_size * self.flops
+        return comp, update
--- models/utils/args.py
+++ models/utils/args.py
@@ -1,75 +1,103 @@
 import argparse
 
-DATASETS = ['sent140', 'femnist', 'shakespeare', 'celeba', 'synthetic', 'reddit']
-SIM_TIMES = ['small', 'medium', 'large']
+DATASETS = ["sent140", "femnist", "shakespeare", "celeba", "synthetic", "reddit"]
+SIM_TIMES = ["small", "medium", "large"]
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-dataset',
-                    help='name of dataset;',
-                    type=str,
-                    choices=DATASETS,
-                    required=True)
-    parser.add_argument('-model',
-                    help='name of model;',
-                    type=str,
-                    required=True)
-    parser.add_argument('--num-rounds',
-                    help='number of rounds to simulate;',
-                    type=int,
-                    default=-1)
-    parser.add_argument('--eval-every',
-                    help='evaluate every ____ rounds;',
-                    type=int,
-                    default=-1)
-    parser.add_argument('--clients-per-round',
-                    help='number of clients trained per round;',
-                    type=int,
-                    default=-1)
-    parser.add_argument('--batch-size',
-                    help='batch size when clients train on data;',
-                    type=int,
-                    default=10)
-    parser.add_argument('--seed',
-                    help='seed for random client sampling and batch splitting',
-                    type=int,
-                    default=0)
-    parser.add_argument('--metrics-name', 
-                    help='name for metrics file;',
-                    type=str,
-                    default='metrics',
-                    required=False)
-    parser.add_argument('--metrics-dir', 
-                    help='dir for metrics file;',
-                    type=str,
-                    default='metrics',
-                    required=False)
-    parser.add_argument('--use-val-set', 
-                    help='use validation set;', 
-                    action='store_true')
+    parser.add_argument(
+        "-dataset", help="name of dataset;", type=str, choices=DATASETS, required=True
+    )
+    parser.add_argument("-model", help="name of model;", type=str, required=True)
+    parser.add_argument(
+        "--num-rounds", help="number of rounds to simulate;", type=int, default=-1
+    )
+    parser.add_argument(
+        "--eval-every", help="evaluate every ____ rounds;", type=int, default=-1
+    )
+    parser.add_argument(
+        "--clients-per-round",
+        help="number of clients trained per round;",
+        type=int,
+        default=-1,
+    )
+    parser.add_argument(
+        "--batch-size",
+        help="batch size when clients train on data;",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--seed",
+        help="seed for random client sampling and batch splitting",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--metrics-name",
+        help="name for metrics file;",
+        type=str,
+        default="metrics",
+        required=False,
+    )
+    parser.add_argument(
+        "--metrics-dir",
+        help="dir for metrics file;",
+        type=str,
+        default="metrics",
+        required=False,
+    )
+    parser.add_argument(
+        "--use-val-set", help="use validation set;", action="store_true"
+    )
 
     # Minibatch doesn't support num_epochs, so make them mutually exclusive
     epoch_capability_group = parser.add_mutually_exclusive_group()
-    epoch_capability_group.add_argument('--minibatch',
-                    help='None for FedAvg, else fraction;',
-                    type=float,
-                    default=None)
-    epoch_capability_group.add_argument('--num-epochs',
-                    help='number of epochs when clients train on data;',
-                    type=int,
-                    default=1)
+    epoch_capability_group.add_argument(
+        "--minibatch", help="None for FedAvg, else fraction;", type=float, default=None
+    )
+    epoch_capability_group.add_argument(
+        "--num-epochs",
+        help="number of epochs when clients train on data;",
+        type=int,
+        default=1,
+    )
 
-    parser.add_argument('-t',
-                    help='simulation time: small, medium, or large;',
-                    type=str,
-                    choices=SIM_TIMES,
-                    default='large')
-    parser.add_argument('-lr',
-                    help='learning rate for local optimizers;',
-                    type=float,
-                    default=-1,
-                    required=False)
-
+    parser.add_argument(
+        "-t",
+        help="simulation time: small, medium, or large;",
+        type=str,
+        choices=SIM_TIMES,
+        default="large",
+    )
+    parser.add_argument(
+        "-lr",
+        help="learning rate for local optimizers;",
+        type=float,
+        default=-1,
+        required=False,
+    )
+    parser.add_argument(
+        "--noise_multiplier",
+        help="Noise multiplier for Gaussian used in DP-SGD",
+        type=float,
+        default=1.1,  # from https://github.com/tensorflow/privacy/blob/master/tutorials/mnist_dpsgd_tutorial.py
+    )
+    parser.add_argument(
+        "--l2_norm_clip", help="Clipping norm for DP-SGD", type=float, default=1.0
+    )
+    parser.add_argument(
+        "--num_microbatches",
+        help="Number of microbatches for DP-SGD",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "--unroll_microbatches",
+        help="Unroll microbatches for DP-SGD?",
+        action="store_true",
+        default=False,
+    )
     return parser.parse_args()
